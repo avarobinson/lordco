e2fa795cd627c0f882adc92b0fcf326d9cbc0b76
Ava Robinson <avamrobinson@gmail.com>
2019-03-24 00:32:58 -0700

    form request update

--- javascriopt.js
+++ javascriopt.js
@@ -5,3 +5,7 @@ function hover(ele) {
 function unhover(ele){
 	ele.style.opacity = "1";
 }
+
+function request_form(ele){
+	document.getElementsByClassName("form-elements")[0].style.display = "inline";
+}
